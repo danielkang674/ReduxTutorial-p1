48333afee13f7d1ff80c78ed963d27bdb62b481f
Daniel Kang <danielkang674@gmail.com>
2018-11-16 17:27:39 -0600

    Toggle todos

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const todos = (state = [], action) => {
           text: action.text,
           completed: false
         }
-      ]
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
     default:
-      return state
+      return state;
   }
 };
 
@@ -35,7 +42,43 @@ const testAddTodo = () => {
   expect(todos(stateBefore, action)).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ];
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('all tests passed');
 
 
