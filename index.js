2dd2b022b840cec311b336ac4abc020cbe60034c
Daniel Kang <danielkang674@gmail.com>
2018-11-17 17:42:56 -0600

    Add redux and created store

--- index.js
+++ index.js
@@ -1,25 +1,86 @@
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id === action.id) {
+        return { ...state, completed: !state.completed }
+      }
+      return state;
+    default:
+      return state;
+  }
+};
+
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ];
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id === action.id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      });
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
 };
+
+const todosApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+  };
+};
+
+const { createStore } = Redux;
+const store = createStore(todosApp);
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping'
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 1
+});
+
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
+});
+
+console.log(store.getState());
 
 const testAddTodo = () => {
   const stateBefore = [];
